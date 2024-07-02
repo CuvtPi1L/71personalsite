e0335c49bc0f2c7b3e42afd24bbd66667f86443f
Sean CHen <Seanchendu@gmail.com>
2024-07-02 23:28:59 +0800

    dog

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -4,13 +4,18 @@ import myPicture from "../assets/profilePiv.png"
 
 export default function PersonalInfo(){
   return(
-    <div className="PersonalInfo">
+    <div id="PersonalInfo">
       <div>
         <h1> Sean Chen</h1>
-        <p>Welcome to my personal site, there is not much here right now, but if you come back later, there will be more items. </p>
+        <p>Welcome to my personal site, there is not much here right now, but if you come back later, it's only going to get better </p>
+        <p>here is a picture of mine</p>
+        <p>see you in a week?</p>
+
+
+
       </div>
 
-      <img src={myPicture} alt="myPicture" width="60px" />
+      <img src={myPicture} alt="myPicture" width="100px" />
     </div>
   )
 }
