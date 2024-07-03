5d294b6027defb2cc3dc5f4b1b498fcc837ffff7
Sean CHen <Seanchendu@gmail.com>
2024-07-03 16:56:00 +0800

    markdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import PersonalInfo from "./components/PersonalInfo"
+import ReactMarkdown from "react-markdown"
 
 export default function App(){
+
+
+  const markdowntest = `
+  
+  # testing markdown
+  this would be *italic*, and this would be **bold**`;
+  
+
+
   return(
     <>
       <PersonalInfo/>
+      <ReactMarkdown>
+        {markdowntest}
+      </ReactMarkdown>
+
     </>
   )
 }
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import 
 
 export default function Blog(){
   return(
     <>
-      
+      <div>
+        
+      </div>
     </>
   )
 }
